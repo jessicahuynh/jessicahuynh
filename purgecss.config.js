8ae96a4d5d20fd07f5515dcfc1e9b532949f8577
Jessica Huynh <jessicahhuynh@outlook.com>
2020-06-21 21:59:07 -0400

    purge unused styles with purgecss

---
+++ purgecss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    content: ['layouts/*.html', 'layouts/*/*.html', 'static/js/*.js'],
+    css: ['static/css/styles.combined.css'],
+    whitelistPatternsChildren: [/chroma$/]
+}
